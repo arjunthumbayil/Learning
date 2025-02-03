0b42b20f68c34d3491a07e2b4098c0fb79595a31
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-02-04 00:16:57 +0530

    parcel bundler dev

---
+++ 2_Episode/App.js
@@ -0,0 +1,27 @@
+import React from "react"
+import ReactDOM from "react-dom" 
+
+const reactHeading = React.createElement(
+  'h1',
+  { id: 'heading' },
+  'Hello From React'
+)
+
+const parent = React.createElement(
+  'div',
+  { id: 'parent' },[
+  React.createElement(
+    'div',
+    { id: 'child' },
+    React.createElement('h1', { id: 'parent' }, 'I am h1')
+  ), React.createElement(
+    'div',
+    { id: 'child' },
+    React.createElement('h1', { id: 'parent' }, 'I am h1')
+  )
+])
+
+const reactRoot = ReactDOM.createRoot(document.getElementById('root3'))
+reactRoot.render(parent)
+
+console.log('arjun', parent)
