dcc8f0b6f992eb00d838ff1940946da4488d2dee
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-02-04 23:36:06 +0530

    jsx intro

--- 3_Episode/App.js
+++ 3_Episode/App.js
@@ -1,12 +1,24 @@
-import React from "react"
-import ReactDOM from "react-dom/client" 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 //React.createElement is an object and not html tag
-//When we render this element onto the DOM, ReactDOM CONVERTS this object to HTML  
+//When we render this element onto the DOM, ReactDOM CONVERTS this object to HTML
 //and pushes it to browser i.e REPLACES everithing inside the root.
 
-const heading = React.createElement("h1", {id:"heading",}, "Hi from React & Parcel!");
+const heading = React.createElement(
+  'h1',
+  { id: 'heading' },
+  'Hi from React & Parcel!'
+)
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//JSX - is not HTML in JS
+//It is at max HTML or XML like syntax
 
-root.render(heading);
+const jsxHeading = <h1 id="heading">Hello React using JSX</h1>
+
+console.log("jsx" + jsxHeading)
+console.log("react" + heading)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(jsxHeading)
