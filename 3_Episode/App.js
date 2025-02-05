77001280fd039c1b8f264e5625f7d1b5a38ab5a6
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-02-06 02:39:38 +0530

    43

--- 3_Episode/App.js
+++ 3_Episode/App.js
@@ -1,19 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//React.createElement is an object and not html tag
-//When we render this element onto the DOM, ReactDOM CONVERTS this object to HTML
-//and pushes it to browser i.e REPLACES everithing inside the root.
-
-//JSX - is not HTML in JS
-//It is at max HTML or XML like syntax
-
-//So it's like React.createElement => ReactElement-JS Object => HTMLElement(render)
-//JSX is transpiled before it reaches the JS Engine. Parcel - Babel.
-
-//JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
-
-const jsxHeading = (
+const heading = (
   <h1 className="head" id="heading">
     Hello React using JSX
   </h1>
@@ -21,4 +9,10 @@ const jsxHeading = (
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(jsxHeading)
+//React Functional Component - is a normal JS Function which returns some JSX
+
+const HeadingComponent = () => {
+  return <h1>Hello from React Functional Component</h1>
+}
+
+root.render(heading)
