8b668cfb3e5923a2da46df44ae9ed87fd553e099
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-02-06 19:02:57 +0530

    Foody App first commit

---
+++ 4_Episode/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+/*
+Foody App Layout
+Header
+-Logo
+-Nav Items
+Body
+-SearchBar
+-Restruant Container => Restraunt Cards
+Footer
+-Copyrights -Links -Address -Contact etc 
+*/
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render()
