cf264820ab3803823a9e2090419fc16d7189a6f6
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-02-09 12:56:17 +0530

    mock data removed plus cleansing done

--- 6_Episode/src/components/Body.js
+++ 6_Episode/src/components/Body.js
@@ -1,25 +1,14 @@
 import RestaurantCard from './RestaurantCard';
-import resList from '../utils/mockData';
 import { useState, useEffect } from 'react';
+import { url3 } from '../utils/constants';
 
 const Body = () => {
-  const [listOfRes, setListOfRes] = useState(resList);
+  const [listOfRes, setListOfRes] = useState([]);
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  const url1 =
-    'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&collection=83639&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null';
-
-  const url2 =
-    'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
-
-  const url3 =
-    'https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.9187787&lng=79.8163671&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
-
-  const url4 =
-    'https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.2587531&lng=75.78041&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
   const fetchData = async () => {
     const data = await fetch(url3);
 
