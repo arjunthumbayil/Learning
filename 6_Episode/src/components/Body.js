90839626d31effb9de6f3e6d17a38efffd326c44
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-02-09 12:49:47 +0530

    filter 4.3+ restaurants from given city api-check

--- 6_Episode/src/components/Body.js
+++ 6_Episode/src/components/Body.js
@@ -1,13 +1,38 @@
 import RestaurantCard from './RestaurantCard';
 import resList from '../utils/mockData';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Body = () => {
   const [listOfRes, setListOfRes] = useState(resList);
 
   useEffect(() => {
-    console.log('useEffect Called');
+    fetchData();
   }, []);
+
+  const url1 =
+    'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&collection=83639&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null';
+
+  const url2 =
+    'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+
+  const url3 =
+    'https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.9187787&lng=79.8163671&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+
+  const url4 =
+    'https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.2587531&lng=75.78041&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+  const fetchData = async () => {
+    const data = await fetch(url3);
+
+    const json = await data.json();
+
+    const requiredData =
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
+
+    console.log(requiredData);
+
+    setListOfRes(requiredData);
+  };
+
   return (
     <div className="body">
       <div className="filter">
--- 6_Episode/src/components/RestaurantCard.js
+++ 6_Episode/src/components/RestaurantCard.js
@@ -1,9 +1,17 @@
-import { CDN_URL } from '../utils/constants'
+import { CDN_URL } from '../utils/constants';
 
 const RestrauntCard = (props) => {
-  const { resData } = props
-  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } =
-    resData?.info
+  const { resData } = props;
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    locality,
+    areaName,
+    avgRating,
+    costForTwo,
+    sla,
+  } = resData?.info;
   return (
     <div className="res-card">
       <img
@@ -14,10 +22,12 @@ const RestrauntCard = (props) => {
       <h3>{name}</h3>
       <h4>{cuisines.join(', ')}</h4>
       <h4>{avgRating} stars</h4>
+      <h4>Area: {locality}</h4>
+      <h4>Location: {areaName}</h4>
       <h4>{costForTwo}</h4>
       <h4>{sla.deliveryTime} minutes</h4>
     </div>
-  )
-}
+  );
+};
 
-export default RestrauntCard
+export default RestrauntCard;
