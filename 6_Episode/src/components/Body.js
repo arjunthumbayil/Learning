0b6b0010c6f3aa5fc547a5a30995dcbd01042bb9
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-02-09 09:51:25 +0530

    useEffect introduced

---
+++ 6_Episode/src/components/Body.js
@@ -0,0 +1,37 @@
+import RestaurantCard from './RestaurantCard';
+import resList from '../utils/mockData';
+import { useState } from 'react';
+
+const Body = () => {
+  const [listOfRes, setListOfRes] = useState(resList);
+
+  useEffect(() => {
+    console.log('useEffect Called');
+  }, []);
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRes.filter(
+              (res) => res.info.avgRating > 4.3
+            );
+            setListOfRes(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRes.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
