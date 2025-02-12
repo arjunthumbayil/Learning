a06c17b6b535e4faeb68e9e04c2765c1a53f67e2
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-02-12 13:40:36 +0530

    OutLet component from ReactRouterDom added for handling children routes

--- 7_Episode/src/App.js
+++ 7_Episode/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppLayout from './components/AppLayout';
+import Body from './components/Body';
 import About from './components/About';
 import Contact from './components/Contact';
 import Error from './components/Error';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const appRouter = createBrowserRouter([
-  { path: '/', element: <AppLayout />, errorElement: <Error /> },
-  { path: '/about', element: <About /> },
-  { path: '/contact', element: <Contact /> },
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      { path: '/', element: <Body /> },
+      { path: '/about', element: <About /> },
+      { path: '/contact', element: <Contact /> },
+    ],
+    errorElement: <Error />,
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- 7_Episode/src/components/AppLayout.js
+++ 7_Episode/src/components/AppLayout.js
@@ -1,13 +1,13 @@
-import Header from './Header'
-import Body from './Body'
+import Header from './Header';
+import { Outlet } from 'react-router-dom';
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default AppLayout
+export default AppLayout;
