ca07075473c24f0e211d397da77b7c0e972140e0
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-02-18 18:22:11 +0530

    class component added inside about page

--- 8_Episode/src/components/About.js
+++ 8_Episode/src/components/About.js
@@ -1,10 +1,12 @@
 import User from './User';
+import UserClass from './UserClass';
 
 const About = () => {
   return (
     <div>
       <h1>This is About page!</h1>
       <User />
+      <UserClass />
     </div>
   );
 };
---
+++ 8_Episode/src/components/UserClass.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class userClass extends React.Component {
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name: John</h2>
+        <h3>Location: Kolkata</h3>
+        <h4>Contact: @John.xyz</h4>
+      </div>
+    );
+  }
+}
+
+export default userClass;
