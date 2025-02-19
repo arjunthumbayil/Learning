71aa6b75d47c4977ddaf48cdcf535062c5730ea6
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-02-19 22:32:56 +0530

    state variable inside class based component after destructuring

--- 8_Episode/src/components/UserClass.js
+++ 8_Episode/src/components/UserClass.js
@@ -8,9 +8,10 @@ class userClass extends React.Component {
   }
   render() {
     const { name, location } = this.props;
+    const { count } = this.state;
     return (
       <div className="user-card">
-        <h2>CountClass: {this.state.count}</h2>
+        <h2>CountClass: {count}</h2>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: @John.xyz</h4>
