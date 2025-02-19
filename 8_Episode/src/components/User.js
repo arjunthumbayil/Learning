08fb2a3fcebba8b9a32e5c48b9b85170653c55e3
Arjun Thumbayil <arjunthumbayil@gmail.com>
2025-02-19 22:37:03 +0530

    having multiple state variables inside class based components

--- 8_Episode/src/components/User.js
+++ 8_Episode/src/components/User.js
@@ -2,9 +2,12 @@ import { useState } from 'react';
 
 const User = (props) => {
   const [count] = useState(0);
+  const [count2] = useState(1);
+
   return (
     <div className="user-card">
       <h2>Count: {count}</h2>
+      <h2>Count_2: {count2}</h2>
       <h2>Name: {props.name}</h2>
       <h3>Location: Kolkata</h3>
       <h4>Contact: @John.xyz</h4>
--- 8_Episode/src/components/UserClass.js
+++ 8_Episode/src/components/UserClass.js
@@ -4,14 +4,15 @@ class userClass extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { count: 0 };
+    this.state = { count: 0, count_2: 1 };
   }
   render() {
     const { name, location } = this.props;
-    const { count } = this.state;
+    const { count, count_2 } = this.state;
     return (
       <div className="user-card">
         <h2>CountClass: {count}</h2>
+        <h2>CountClass_2: {count_2}</h2>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: @John.xyz</h4>
